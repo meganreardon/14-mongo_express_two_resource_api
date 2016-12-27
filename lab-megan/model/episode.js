49a9ac43ce0e5ba14b172c5dddaa059b38e1c977
Megan Reardon <meganr@gmail.com>
2016-12-27 13:21:37 -0800

    created models, renamed episodes to episode

---
+++ lab-megan/model/episode.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const episodeSchema = Schema({
+  name: { type: String, required: true },
+  director: { type: String, required: true },
+  tvshowID: { type: Schema.Types.ObjectId, required: true }
+});
+
+module.exports = mongoose.model('episode', episodeSchema);
