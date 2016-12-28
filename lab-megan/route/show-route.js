2ee8ed06f2cd771b07d4fa8b0e75b474c7019b7f
Megan Reardon <meganr@gmail.com>
2016-12-28 12:40:18 -0800

    added debug to route pages

--- lab-megan/route/show-route.js
+++ lab-megan/route/show-route.js
@@ -3,11 +3,13 @@
 const Router = require('express').Router;
 const jsonParser = require('body-parser').json();
 const createError = require('http-errors');
+const debug = require('debug')('show:show-router'); // TODO is this right??
 
 const Show = require('../model/show.js');
 const showRouter = module.exports = new Router();
 
 showRouter.post('/api/show', jsonParser, function(req, res, next) {
+  debug('POST: /api/show');
   // req.body.startDate = new Date(); // don't need this
   new Show(req.body).save()
   .then( show => res.json(show))
@@ -15,6 +17,7 @@ showRouter.post('/api/show', jsonParser, function(req, res, next) {
 });
 
 showRouter.get('/api/show/:id', function(req, res, next) {
+  debug('GET: /api/show/:id');
   Show.findById(req.params.id)
   .populate('episodes')
   .then( show => res.json(show))
@@ -22,6 +25,7 @@ showRouter.get('/api/show/:id', function(req, res, next) {
 });
 
 showRouter.put('/api/show/:id', jsonParser, function(req, res, next) {
+  debug('PUT: /api/show/:id');
   Show.findByIdAndUpdate(req.params.id, req.body, { new: true })
   .then( show => res.json(show))
   .catch( err => {
@@ -31,6 +35,7 @@ showRouter.put('/api/show/:id', jsonParser, function(req, res, next) {
 });
 
 showRouter.delete('/api/show/:id', function(req, res, next) {
+  debug('DELETE: /api/show/:id');
   Show.findByIdAndRemove(req.params.id)
   .then( () => res.status(204).send())
   .catch( err => next(createError(404, err.message)));
